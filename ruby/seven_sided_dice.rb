70bdd8a281e1e4276767e6f96f193e10f453f296
Kevin Niu <kev.niu@gmail.com>
2016-09-20 16:22:45 -0700

    Add in place shuffle

--- ruby/seven_sided_dice.rb
+++ ruby/seven_sided_dice.rb
@@ -1,9 +1,22 @@
 # 38 interview cake
 
-def rand5()
-  result = 7 # arbitrarily large
-  while result > 5
-      result = rand7()
-  end
-  return result
+def rand7()
+
+    while true
+
+        # do our die rolls
+        roll1 = rand5()
+        roll2 = rand5()
+
+        outcome_number = (roll1-1) * 5 + (roll2-1) + 1
+
+        # if we hit an extraneous
+        # outcome we just re-roll
+        if outcome_number > 21
+            next
+        end
+
+        # our outcome was fine. return it!
+        return outcome_number % 7 + 1
+    end
 end
---
+++ ruby/shuffle.rb
@@ -0,0 +1,27 @@
+# 35 inteview cake
+
+def get_random(floor, ceiling)
+    rand(floor..ceiling)
+end
+
+def shuffle(the_array)
+    # if it's 1 or 0 items, just return
+    if the_array.length <= 1
+        return the_array
+    end
+
+    last_index_in_the_array = the_array.length - 1
+
+    # walk through from beginning to end
+    (0...the_array.length).each do |index_we_are_choosing_for|
+
+        # choose a random not-yet-placed item to place there
+        # (could also be the item currently in that spot)
+        # must be an item AFTER the current item, because the stuff
+        # before has all already been placed
+        random_choice_index = get_random(index_we_are_choosing_for, last_index_in_the_array)
+
+        # place our random choice in the spot by swapping
+        the_array[index_we_are_choosing_for], the_array[random_choice_index] = the_array[random_choice_index], the_array[index_we_are_choosing_for]
+    end
+end
